279ad5ea98771336b25a6ff76150bd540577c63e
youssefhammani <youssefhammani04@gmail.com>
2024-01-24 17:58:17 +0100

    Install a package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,6 @@
 # Puppet manifest to install Flask package
 
-exec { 'puppet-lint':
-  command => '/usr/bin/apt-get -y install puppet-lint -v 2.5.0',
+package { 'Flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
 }
