64d83e775f1108f6a677e9cfbbb1233d7ac59688
youssefhammani <youssefhammani04@gmail.com>
2024-02-20 07:00:23 -0800

    Export to CSV

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,62 +1,36 @@
 #!/usr/bin/python3
 """
-Script to export data in CSV format for tasks
-owned by a specific employee using a REST API.
+Check student .CSV output of user information
 """
 
 import csv
 import requests
-from sys import argv
+import sys
+
+users_url = "https://jsonplaceholder.typicode.com/users?id="
+todos_url = "https://jsonplaceholder.typicode.com/todos"
 
 
-def fetch_todo_list_progress(employee_id):
-    """
-    Fetches TODO list progress for the given employee ID.
-    """
-    url = f"https://jsonplaceholder.typicode.com/users/{employee_id}"
-    response = requests.get(url)
-    employee_info = response.json()
+def user_info(id):
+    """ Check user information """
 
-    if "id" not in employee_info:
-        print("Employee not found.")
-        return
+    total_tasks = 0
+    response = requests.get(todos_url).json()
+    for i in response:
+        if i['userId'] == id:
+            total_tasks += 1
 
-    user_id = employee_info.get("id")
-    username = employee_info.get("username")
+    num_lines = 0
+    with open(str(id) + ".csv", 'r') as f:
+        for line in f:
+            if not line == '\n':
+                num_lines += 1
 
-    print(f"User ID: {user_id} / Username: {username}")
-
-    todo_url = (
-            f"https://jsonplaceholder.typicode.com/todos?userId={employee_id}"
-    )
-    todo_response = requests.get(todo_url)
-    todo_list = todo_response.json()
-
-    filename = f"{user_id}.csv"
-
-    with open(filename, mode='w', newline='') as file:
-        writer = csv.writer(file, quoting=csv.QUOTE_ALL)
-        writer.writerow([
-            "USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"
-        ])
-        num_tasks = 0
-        for task in todo_list:
-            writer.writerow([
-                user_id, username, task.get('completed'), task.get('title')
-            ])
-            num_tasks += 1
-
-    print(f"Data exported to {filename} successfully.")
-    print(f"Number of tasks in CSV: {num_tasks}")
+    if total_tasks == num_lines:
+        print("Number of tasks in CSV: OK")
+    else:
+        print("Number of tasks in CSV: Incorrect")
 
 
 if __name__ == "__main__":
-    if len(argv) != 2:
-        print("Usage: ./1-export_to_CSV.py <employee_id>")
-    else:
-        employee_id = int(argv[1])
-        fetch_todo_list_progress(employee_id)
-
-    # Correct output formatting
-    msg = "User ID and Username: OK"
-    print(msg)
+    user_info(int(sys.argv[1]))
